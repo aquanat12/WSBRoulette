117195f4c383f3666da0c81fea24efd6ff2ef072
nigel vdh <vdhorstnigel@gmail.com>
2021-04-14 22:14:58 +0800

    Added Favicon.ico and qood working in progress

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const http = require('http')
-const fs = require('fs')
-const express = require('express')
-const port = 80
-const app = express()
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const path = require('path')
+const port = 80;
+const app = express();
+const bodyParser = require("body-parser"); 
+const fetch = require('node-fetch');
+const FAVICON = path.join(__dirname, 'assets', 'favicon.ico');
+
+
 
 const server = http.createServer(function (req, res) {
+    if (req.url === '/favicon.ico') {
+    res.setHeader('Content-Type', 'image/x-icon');
+    fs.createReadStream(FAVICON).pipe(res);
+    return;}
     res.writeHead(200, { 'Content-Type': 'text/html' })
     fs.readFile('index.html', function (error, data) {
         if (error) {
@@ -17,6 +27,16 @@ const server = http.createServer(function (req, res) {
     })
 })
 
+
+var qood = fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    var randomNumber = Math.floor(Math.random()*data.length);
+    const qoodtext = data[randomNumber].text;
+    const qoodauthor = data[randomNumber].author;
+  });
 
 
 server.listen(port, function (error) {
