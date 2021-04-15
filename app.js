e46db0d8518faee4a8653f2d988a8b6ed7bdb0cd
nigel vdh <vdhorstnigel@gmail.com>
2021-04-15 22:02:44 +0800

    favicon working

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-var app = require("express")(); 
-var bodyParser = require("body-parser"); 
+const app = require("express")(); 
+const fetch = require('node-fetch');
+const bodyParser = require("body-parser"); 
+const favicon = require('serve-favicon');
 app.set("view engine", "ejs"); 
 app.set("views", __dirname + "/views"); 
 app.use(bodyParser.urlencoded({ extended: false })); 
-app.get("/", (req, res) => { res.render("index") }); 
-app.listen(80);
+app.use(favicon(__dirname + '/assets/favicon.ico')); 
 
+
+
+async function qoodresult(){
+const r = await qood;
+var qoodtext = r.text;}
+console.log(qoodresult());
+app.get("/", (req, res) => 
+        {
+            var qoodtext = "hi";
+            res.render("index", { qoodtext: qoodtext });
+        }); 
+app.listen(80);
 
 
 var qood = fetch("https://type.fit/api/quotes")
