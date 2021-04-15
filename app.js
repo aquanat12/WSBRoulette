441142531885eb8e7a3af7ad86e212a1b7d219d8
nigel vdh <vdhorstnigel@gmail.com>
2021-04-15 22:19:41 +0800

    Qood finally done

--- app.js
+++ app.js
@@ -8,26 +8,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(favicon(__dirname + '/assets/favicon.ico')); 
 
 
-
-async function qoodresult(){
-const r = await qood;
-var qoodtext = r.text;}
-console.log(qoodresult());
 app.get("/", (req, res) => 
         {
-            var qoodtext = "hi";
-            res.render("index", { qoodtext: qoodtext });
+        var qood = fetch("https://type.fit/api/quotes")
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            var randomNumber = Math.floor(Math.random()*data.length);
+            var  qoodtext = data[randomNumber].text;
+            var  qoodauthor = data[randomNumber].author;
+            res.render("index", { qoodtext: qoodtext, qoodauthor: qoodauthor});
+          });
         }); 
 app.listen(80);
 
 
-var qood = fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    var randomNumber = Math.floor(Math.random()*data.length);
-    return data[randomNumber];
-  });
+
 
 
