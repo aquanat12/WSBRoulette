ba73fcbd96050100683a119c160e2636b27af4d0
nigel vdh <vdhorstnigel@gmail.com>
2021-04-17 14:31:12 +0800

    Back to Rss with 100 items and regex for finding tickers

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const app = require("express")();
 const fetch = require('node-fetch');
 const bodyParser = require("body-parser"); 
 const favicon = require('serve-favicon');
-const puppeteer = require('puppeteer');
+const Parser = require('rss-parser');
 var parser = new Parser();
-const Url = 'https://stocks.comment.ai/trending.html';
 app.set("view engine", "ejs"); 
 app.set("views", __dirname + "/views"); 
 app.use(bodyParser.urlencoded({ extended: false })); 
@@ -23,7 +22,16 @@ app.get("/", (req, res) =>
             res.render("index", { qoodtext: qoodtext, qoodauthor: qoodauthor});
           });
             }); 
+    (async () => {
+        var symbol_pat = /(^|\s)(\$[a-z]+\b)/gi;
+        let feed = await parser.parseURL('https://www.reddit.com/r/wallstreetbets/.rss?limit=100');
+        feed.items.forEach(item => {
+            var ticker = item.title.match(symbol_pat);
+            console.log(ticker);
+            console.log(item.title + ':' + item.link)
+        });
 
+    })();
 app.listen(80);
 
 
