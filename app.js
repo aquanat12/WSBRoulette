5778e3811c3970466fb5230139f8f2ce8b9a2cee
nigel vdh <vdhorstnigel@gmail.com>
2021-04-18 11:31:27 +0800

    Got regex done and check if ticker is valid

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const fetch = require('node-fetch');
 const bodyParser = require("body-parser"); 
 const favicon = require('serve-favicon');
 const Parser = require('rss-parser');
-var parser = new Parser();
+const parser = new Parser();
 app.set("view engine", "ejs"); 
 app.set("views", __dirname + "/views"); 
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(favicon(__dirname + '/assets/favicon.ico')); 
+
 
 app.get("/", (req, res) => 
         {
@@ -23,12 +24,23 @@ app.get("/", (req, res) =>
           });
             }); 
     (async () => {
-        var symbol_pat = /(^|\s)(\$[a-z]+\b)/gi;
+        var symbol_pat = /(^\$[A-Z]{3,5})|([A-Z]{3,5})/;
         let feed = await parser.parseURL('https://www.reddit.com/r/wallstreetbets/.rss?limit=100');
         feed.items.forEach(item => {
             var ticker = item.title.match(symbol_pat);
-            console.log(ticker);
-            console.log(item.title + ':' + item.link)
+            if (ticker != null) {
+                var cts = fetch("https://ticker-2e1ica8b9.now.sh/keyword/" + ticker[0])
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        if (data.length != 0) {
+                            console.log(data[0].symbol);
+                            console.log(data[0].name);
+                        }
+                    });
+            };
+            //console.log(item.title + ':' + item.link);
         });
 
     })();
