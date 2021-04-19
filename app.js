c0cdccd3d10423d4088a6586ab6de3323b95e957
nigel vdh <vdhorstnigel@gmail.com>
2021-04-19 21:27:58 +0800

    Updated regex to as best i can. Now it only checks if there 3-5 capital letters within the word boundary

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const parser = new Parser();
 app.set("view engine", "ejs"); 
 app.set("views", __dirname + "/views"); 
 app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(favicon(__dirname + '/assets/favicon.ico')); 
+app.use(favicon(__dirname + '/assets/favicon.ico'));
+var qoodapi = "https://type.fit/api/quotes";
+var rssapi = "https://www.reddit.com/r/wallstreetbets/.rss?limit=100";
 
 
 app.get("/", (req, res) => 
         {
-        var qood = fetch("https://type.fit/api/quotes")
+        var qood = fetch(qoodapi)
           .then(function(response) {
             return response.json();
           })
@@ -24,19 +26,27 @@ app.get("/", (req, res) =>
           });
             }); 
     (async () => {
-        var symbol_pat = /(^\$[A-Z]{3,5})|([A-Z]{3,5})/;
-        let feed = await parser.parseURL('https://www.reddit.com/r/wallstreetbets/.rss?limit=100');
+        var symbol_pat = /(\b[A-Z]{3,5}\b)/g;
+        let feed = await parser.parseURL(rssapi);
         feed.items.forEach(item => {
             var ticker = item.title.match(symbol_pat);
             if (ticker != null) {
+                console.log (ticker);
                 var cts = fetch("https://ticker-2e1ica8b9.now.sh/keyword/" + ticker[0])
                     .then(function (response) {
                         return response.json();
                     })
                     .then(function (data) {
                         if (data.length != 0) {
-                            console.log(data[0].symbol);
-                            console.log(data[0].name);
+                            console.log(item.title + " : " + ticker[0]);
+                            var stocks = [];
+                            var stocksarray = stocks.concat(data);
+                            //console.log(stocksarray.length);    
+                            //console.log(stocks.length);
+                            var randomNumber = Math.floor(Math.random()*data.length);
+                            var  randticker = data[randomNumber].symbol;
+                            var  randname = data[randomNumber].name;
+                            //res.render("index", { qoodtext: qoodtext, qoodauthor: qoodauthor});
                         }
                     });
             };
