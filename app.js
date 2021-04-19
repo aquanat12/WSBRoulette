23fa1307a1d49d647175f7bdbf9f4255a0c36f4f
nigel vdh <vdhorstnigel@gmail.com>
2021-04-19 23:04:28 +0800

    Tickers are now sorted in an array which counts how many there are, exclude mentions that are less than 2 to prevent random tickers from entering and exclude WSB,YOLO,DFV.

--- app.js
+++ app.js
@@ -4,13 +4,46 @@ const bodyParser = require("body-parser");
 const favicon = require('serve-favicon');
 const Parser = require('rss-parser');
 const parser = new Parser();
+const StockSocket = require("stocksocket");
 app.set("view engine", "ejs"); 
 app.set("views", __dirname + "/views"); 
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(favicon(__dirname + '/assets/favicon.ico'));
 var qoodapi = "https://type.fit/api/quotes";
 var rssapi = "https://www.reddit.com/r/wallstreetbets/.rss?limit=100";
+var stocks = [];
 
+// edited from https://gist.github.com/ralphcrisostomo/3141412
+function compressArray(original) {
+ 
+	var compressed = [];
+	// make a copy of the input array
+	var copy = original.slice(0);
+ 
+	// first loop goes over every element
+	for (var i = 0; i < original.length; i++) {
+ 
+		var myCount = 0;	
+		// loop over every element in the copy and see if it's the same
+		for (var w = 0; w < copy.length; w++) {
+			if (original[i] == copy[w]) {
+				// increase amount of times duplicate is found
+				myCount++;
+				// sets item to undefined
+				delete copy[w];
+			}
+		}
+ 
+		if (myCount > 1) {
+			var a = new Object();
+			a.value = original[i];
+			a.count = myCount;
+			compressed.push(a);
+		}
+	}
+ 
+	return compressed;
+};
 
 app.get("/", (req, res) => 
         {
@@ -31,28 +64,21 @@ app.get("/", (req, res) =>
         feed.items.forEach(item => {
             var ticker = item.title.match(symbol_pat);
             if (ticker != null) {
-                console.log (ticker);
-                var cts = fetch("https://ticker-2e1ica8b9.now.sh/keyword/" + ticker[0])
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        if (data.length != 0) {
-                            console.log(item.title + " : " + ticker[0]);
-                            var stocks = [];
-                            var stocksarray = stocks.concat(data);
-                            //console.log(stocksarray.length);    
-                            //console.log(stocks.length);
-                            var randomNumber = Math.floor(Math.random()*data.length);
-                            var  randticker = data[randomNumber].symbol;
-                            var  randname = data[randomNumber].name;
-                            //res.render("index", { qoodtext: qoodtext, qoodauthor: qoodauthor});
-                        }
-                    });
+                var i;
+                for(i = 0; i < ticker.length; i++){
+                    if (ticker[i] != "YOLO" && ticker[i] != "WSB" && ticker[i] != "DFV"){
+                        stocks.push(ticker[i]);
+                    }
+                }
+                                //var randomNumber = Math.floor(Math.random()*data.length);
+                               // var  randticker = data[randomNumber].symbol;
+                               // var  randname = data[randomNumber].name;
+                                //res.render("index", { qoodtext: qoodtext, qoodauthor: qoodauthor});
             };
             //console.log(item.title + ':' + item.link);
         });
-
+        var stocksarray = compressArray(stocks);
+        console.log(stocksarray);
     })();
 app.listen(80);
 
